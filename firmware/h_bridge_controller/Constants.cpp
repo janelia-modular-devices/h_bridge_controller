2f52b02371daea621ce2a437ef6b569df2a69ab6
Peter Polidoro <peterpolidoro@gmail.com>
2016-05-20 16:44:14 -0400

    Progress towards working with new ModularServer.

--- firmware/h_bridge_controller/Constants.cpp
+++ firmware/h_bridge_controller/Constants.cpp
@@ -10,22 +10,22 @@
 
 namespace constants
 {
-const unsigned int baudrate = 9600;
+const size_t baudrate = 9600;
 
-const unsigned int model_number = 1170;
+const size_t model_number = 1170;
 
 // Use semantic versioning http://semver.org/
-const unsigned char firmware_major = 0;
-const unsigned char firmware_minor = 1;
-const unsigned char firmware_patch = 0;
+const long firmware_major = 0;
+const long firmware_minor = 1;
+const long firmware_patch = 0;
 
-const int enable_pins[BRIDGE_COUNT] = {8,9,10,11};
-const int dir_a_pins[BRIDGE_COUNT] = {30,32,34,36};
-const int dir_b_pins[BRIDGE_COUNT] = {31,33,35,37};
-const int sense_pins[BRIDGE_COUNT] = {0,1,2,3};
+const size_t enable_pins[BRIDGE_COUNT] = {8,9,10,11};
+const size_t dir_a_pins[BRIDGE_COUNT] = {30,32,34,36};
+const size_t dir_b_pins[BRIDGE_COUNT] = {31,33,35,37};
+const size_t sense_pins[BRIDGE_COUNT] = {0,1,2,3};
 
-const int di_pins[DIGITAL_INPUT_COUNT] = {21,20,2};
-const int do_pins[DIGITAL_OUTPUT_COUNT] = {48,3};
+const size_t di_pins[DIGITAL_INPUT_COUNT] = {21,20,2};
+const size_t do_pins[DIGITAL_OUTPUT_COUNT] = {48,3};
 
 const uint32_t duration_min = 1;
 const uint32_t duration_max = 0x7FFFFFFF;
@@ -45,24 +45,34 @@ const int pattern_negative_count_default = 1;
 
 CONSTANT_STRING(device_name,"h_bridge_controller");
 
-CONSTANT_STRING(duration_units_name,"ms");
-
-CONSTANT_STRING(invalid_array_length_error,"Invalid array length.");
-
 CONSTANT_STRING(pattern_positive_count,"pattern_positive_count");
 CONSTANT_STRING(pattern_negative_count,"pattern_negative_count");
 
+// Fields
+
+// Parameters
 CONSTANT_STRING(bridge_parameter_name,"bridge");
+
 CONSTANT_STRING(positive_parameter_name,"positive");
+
 CONSTANT_STRING(duration_parameter_name,"duration");
+CONSTANT_STRING(duration_units_name,"ms");
+
 CONSTANT_STRING(on_duration_parameter_name,"on_duration");
+
 CONSTANT_STRING(period_parameter_name,"period");
+
 CONSTANT_STRING(count_parameter_name,"count");
+
 CONSTANT_STRING(digital_input_parameter_name,"digital_input");
+
 CONSTANT_STRING(polarity_reversed_parameter_name,"polarity_reversed");
+
 CONSTANT_STRING(pulse_enabled_parameter_name,"pulse_enabled");
+
 CONSTANT_STRING(pattern_count_parameter_name,"pattern_count");
 
+// Methods
 CONSTANT_STRING(pulse_method_name,"pulse");
 CONSTANT_STRING(add_pwm_period_on_duration_method_name,"addPwmPeriodOnDuration");
 CONSTANT_STRING(get_digital_input_method_name,"getDigitalInput");
@@ -71,4 +81,8 @@ CONSTANT_STRING(get_pulse_info_method_name,"getPulseInfo");
 CONSTANT_STRING(set_polarity_reversed_method_name,"setPolarityReversed");
 CONSTANT_STRING(set_pattern_positive_count_method_name,"setPatternPositiveCount");
 CONSTANT_STRING(set_pattern_negative_count_method_name,"setPatternNegativeCount");
+
+// Errors
+CONSTANT_STRING(invalid_array_length_error,"Invalid array length.");
+
 }
--- firmware/h_bridge_controller/Constants.h
+++ firmware/h_bridge_controller/Constants.h
@@ -13,7 +13,7 @@
 namespace constants
 {
 //MAX values must be >= 1, >= created/copied count, < RAM limit
-enum{SAVED_VARIABLE_COUNT_MAX=3};
+enum{FIELD_COUNT_MAX=3};
 enum{PARAMETER_COUNT_MAX=16};
 enum{METHOD_COUNT_MAX=16};
 
@@ -22,21 +22,21 @@ enum {INDEXED_BRIDGES_COUNT_MAX=4};
 enum {DIGITAL_INPUT_COUNT=3};
 enum {DIGITAL_OUTPUT_COUNT=2};
 
-extern const unsigned int baudrate;
+extern const size_t baudrate;
 
-extern const unsigned int model_number;
+extern const size_t model_number;
 
-extern const unsigned char firmware_major;
-extern const unsigned char firmware_minor;
-extern const unsigned char firmware_patch;
+extern const long firmware_major;
+extern const long firmware_minor;
+extern const long firmware_patch;
 
-extern const int enable_pins[BRIDGE_COUNT];
-extern const int dir_a_pins[BRIDGE_COUNT];
-extern const int dir_b_pins[BRIDGE_COUNT];
-extern const int sense_pins[BRIDGE_COUNT];
+extern const size_t enable_pins[BRIDGE_COUNT];
+extern const size_t dir_a_pins[BRIDGE_COUNT];
+extern const size_t dir_b_pins[BRIDGE_COUNT];
+extern const size_t sense_pins[BRIDGE_COUNT];
 
-extern const int di_pins[DIGITAL_INPUT_COUNT];
-extern const int do_pins[DIGITAL_OUTPUT_COUNT];
+extern const size_t di_pins[DIGITAL_INPUT_COUNT];
+extern const size_t do_pins[DIGITAL_OUTPUT_COUNT];
 
 extern const uint32_t duration_min;
 extern const uint32_t duration_max;
@@ -55,24 +55,36 @@ extern const int pattern_negative_count_default;
 
 extern ConstantString device_name;
 
-extern ConstantString invalid_array_length_error;
-
 extern ConstantString pattern_positive_count;
 extern ConstantString pattern_negative_count;
 
+// Fields
+// Field values must be long, double, bool, long[], double[], bool[], char[]
+
+// Parameters
+extern ConstantString bridge_parameter_name;
+
+extern ConstantString positive_parameter_name;
+
+extern ConstantString duration_parameter_name;
 extern ConstantString duration_units_name;
 
-extern ConstantString bridge_parameter_name;
-extern ConstantString positive_parameter_name;
-extern ConstantString duration_parameter_name;
 extern ConstantString on_duration_parameter_name;
+
 extern ConstantString period_parameter_name;
+
 extern ConstantString count_parameter_name;
+
 extern ConstantString digital_input_parameter_name;
+
 extern ConstantString polarity_reversed_parameter_name;
+
 extern ConstantString pulse_enabled_parameter_name;
+
 extern ConstantString pattern_count_parameter_name;
 
+
+// Methods
 extern ConstantString pulse_method_name;
 extern ConstantString add_pwm_period_on_duration_method_name;
 extern ConstantString get_digital_input_method_name;
@@ -81,5 +93,9 @@ extern ConstantString get_pulse_info_method_name;
 extern ConstantString set_polarity_reversed_method_name;
 extern ConstantString set_pattern_positive_count_method_name;
 extern ConstantString set_pattern_negative_count_method_name;
+
+// Errors
+extern ConstantString invalid_array_length_error;
+
 }
 #endif
